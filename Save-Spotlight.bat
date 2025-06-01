0a85a25917956286f95640a0c3cd97f3baaa9e9d
Woalis <93735822+Woalis@users.noreply.github.com>
2025-06-01 15:20:20 -0400

    Update Save-Spotlight.bat

--- Save-Spotlight.bat
+++ Save-Spotlight.bat
@@ -1,4 +1,6 @@
-rem https://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
+REM Source https://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
+REM https://blog.danskingdom.com/about/#-license
+REM https://creativecommons.org/licenses/by/4.0/
 @ECHO OFF
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%Save-Spotlight-Win11.ps1
